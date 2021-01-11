7935e9a70fe150595d70164e33b17d96ea96c740
Jin De <91093935@g.mvusd.net>
2021-01-11 20:02:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+//import 
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+    int num1;
+    System.out.println("Hello world!");
+    System.out.println("Pick an int");
+    num1 = kb.nextInt();
+    System.out.println("Todays Lucky number is " + num1);
+  }// end Main method
+}// end Main class
